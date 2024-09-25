8b79ea2b5b85ad31e431e1bfd3c49e6bf0ffe468
Ujjval <ujjval2804@gmail.com>
2024-09-25 15:29:02 +0530

    3

--- lib/Screen/login_page.dart
+++ lib/Screen/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login_Page extends StatefulWidget {
@@ -11,11 +12,45 @@ class _Login_PageState extends State<Login_Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Text('Login Page'),
       ),
       body: Center(
-        child: Text('hello Guys'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                  prefixIcon: Icon(CupertinoIcons.person,color: Colors.deepPurple,),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(CupertinoIcons.eye,color: Colors.deepPurple,),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
+                ),
+              ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.black12)
+              ),
+                onPressed: (){},
+                child: Text('Login',style: TextStyle(color: Colors.black38),)
+            )
+          ],
+        )
       ),
     );
   }
